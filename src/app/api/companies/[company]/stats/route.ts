9d1742950d966928c2c93b4735821d1a61c9b152
quadiez <dduidokdoi@gmail.com>
2024-04-22 14:34:49 +0700

    feat: project structure

--- src/app/api/companies/[company]/stats/route.ts
+++ src/app/api/companies/[company]/stats/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-    // get company stats
+    // get company stats by company from supabase
     // https://nextjs.org/docs/app/building-your-application/routing/route-handlers
     return
 }
